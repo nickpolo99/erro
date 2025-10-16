343bd6d257929d34546692f0b8a10968c23e6b75
nickpolo99 <160550179+nickpolo99@users.noreply.github.com>
2025-10-16 15:46:20 -0300

    Delete arquivo-main (1)/arquivo-main/certy-main/tarefas-spring-java-main directory

--- arquivo-main (1)/arquivo-main/certy-main/tarefas-spring-java-main/src/main/java/com/ifsc/tarefas/model/Status.java
+++
@@ -1,9 +0,0 @@
-package com.ifsc.tarefas.model;
-
-public enum Status {
-    PENDENTE,
-    CONCLUIDA;
-
-    private Status() {
-    }
-}
--- arquivo-main (1)/arquivo-main/certy-main/tarefas-spring-java-main/src/main/java/com/ifsc/tarefas/repository/ArquivoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ifsc.tarefas.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ifsc.tarefas.model.Arquivo;
-
-public interface ArquivoRepository extends JpaRepository<Arquivo, Long> {
-
-}
